11f966d6a0ae3efcb0358d09ecc5f5d4f766d5e1
slooock <kayque.avelar@gmail.com>
2018-08-28 18:15:21 -0300

    Pagamentos Favorecidos Empenhos adicionado cliente 245

--- Coleta/API/main.py
+++ Coleta/API/main.py
@@ -7,14 +7,10 @@ dataInicio = "01/01/2017"
 dataFim = "31/12/2017"
 empenho = Empenho.Empenho()
 
-#Gerenciador.limpaDadosBanco("Empenho")
-#Gerenciador.limpaDadosBanco("Pagamento")
-#Gerenciador.limpaDadosBanco("Cliente")
-#Gerenciador.limpaDadosBanco("Favorecido")
-#listaCidades = Gerenciador.verificaSeRetornaEmpenho()
-listaCidades = Gerenciador.verificaSeRetornaEmpenho()
-for cliente in listaCidades:
-    print(cliente.nome + " "+str(cliente.idcliente ))
-#Gerenciador.recuperaClientesInsereBanco("http://transparencia.portalfacil.com.br/api/clientes?type=json")
-#Gerenciador.recuperaEmpenhoInsereBanco(245,dataInicio,dataFim)
-#Gerenciador.recuperaPagamentoInsereBanco(245,dataInicio,dataFim)
+Gerenciador.limpaDadosBanco("Empenho")
+Gerenciador.limpaDadosBanco("Pagamento")
+Gerenciador.limpaDadosBanco("Cliente")
+Gerenciador.limpaDadosBanco("Favorecido")
+Gerenciador.recuperaClientesInsereBanco("http://transparencia.portalfacil.com.br/api/clientes?type=json")
+Gerenciador.recuperaEmpenhoInsereBanco(245,dataInicio,dataFim)
+Gerenciador.recuperaPagamentoInsereBanco(245,dataInicio,dataFim)
