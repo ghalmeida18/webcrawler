cb1f446e2ade3639b6a483e6865415e2d1eef6b7
slooock <kayque.avelar@gmail.com>
2018-08-30 09:16:12 -0300

    Previnindo problemas de conexão

--- Coleta/API/Gerenciador.py
+++ Coleta/API/Gerenciador.py
@@ -48,103 +48,108 @@ def recuperaEmpenhoInsereBanco(idCliente,dataInicio,dataFim):
 	controlador = True
 
 	contadorEmpenhos = 0
-	pagina = 0
+	pagina = 1
 
 	while controlador:
 
-		pagina = pagina + 1
+
 
 		link = "http://transparencia.portalfacil.com.br/api/empenhos?type=json&idCliente="+str(idCliente)+"&page="+str(pagina)+"&pageSize=100&dtInicio="+dataInicio+"&dtFim="+dataFim
+		print(link)
 		con = conectaBanco()
 		listaEmpenhos = []
-		r = requests.get(link)
-		
-		if r.status_code == 200:
-			reddit_data = json.loads(r.content)
-			if len(reddit_data)==21:
-				print("FIM DE RECUPERACAO DE EMPENHOS")
-				return
+
+		try:
+			print("1")
+			r = requests.get(link)
+			print("2")
+			if r.status_code == 200:
+				reddit_data = json.loads(r.content)
+				if len(reddit_data)==21:
+					print("asdsa")
+					print("FIM DE RECUPERACAO DE EMPENHOS")
+					return
+				for reddit in reddit_data:
+					try:
+
+						empenho = Empenho.Empenho()
+
+						empenho.numero=reddit['NumEmpenho']
+						empenho.especie = reddit['TpEmpenho']
+						empenho.orgao = reddit['NumUnidade']+" "+ reddit['DescUnidade']
+						empenho.projeto = "=========PROJETOTESTE========"
+						empenho.elemento =  reddit['NumDespesa']+" "+ reddit['DescDespesa']
+						empenho.licitacao = reddit['NumLicitacao']+" - "+reddit['DtLicitacao']+" - "+reddit['TpLicitacao']
+
+						empenho.processo = reddit['NumProcesso']
+						empenho.dataEmpenho = reddit['DtEmpenho']
 
 
-			for reddit in reddit_data:
-				try:
+						data  = datetime.strptime(empenho.dataEmpenho,"%d/%m/%Y").strftime("%Y-%m-%d")
 
-					empenho = Empenho.Empenho()
+						empenho.valor = reddit['VlEmpenho']
+						empenho.empenho_Numero = reddit['NumEmpenho']
 
-					empenho.numero=reddit['NumEmpenho']
-					empenho.especie = reddit['TpEmpenho']
-					empenho.orgao = reddit['NumUnidade']+" "+ reddit['DescUnidade']
-					empenho.projeto = "=========PROJETOTESTE========"
-					empenho.elemento =  reddit['NumDespesa']+" "+ reddit['DescDespesa']
-					empenho.licitacao = reddit['NumLicitacao']+" - "+reddit['DtLicitacao']+" - "+reddit['TpLicitacao']
+						empenho.funcao = reddit['NumFuncao']+" - "+reddit['DescFuncao']
+						empenho.subFuncao = reddit['NumSubFuncao']+" - "+reddit['DescSubFuncao']
+						empenho.programa = reddit['NumPrograma']+" - "+reddit['DescPrograma']
+						empenho.destinacao = reddit['NumDestinacao']+" - "+reddit['DescDestinacao']
 
-					empenho.processo = reddit['NumProcesso']
-					empenho.dataEmpenho = reddit['DtEmpenho']
+						#INSERINDO FAVORECIDO
+						empenho.insereFavorecido(reddit['DescFornecedor'],reddit['NumCpfCnpjFornecedor'],"=====CARGO TESTE=====")
+
+						cursor = con.cursor()
+						#INSERE NOVO FAVORECIDO CASO NÃO TENHA NO BANCO DE DADOS
+						if(not(cursor.execute("SELECT * from Favorecido where Nome = '" + empenho.retornaFavorecido().retornaNome() + "'"))):
+							sqlquery = "INSERT INTO Favorecido(CPF_CNPJ, Nome, Cargo,idCliente) VALUES (%s, %s, %s, %s);"
+							cursor.execute(sqlquery,(
+							        empenho.retornaFavorecido().CPF_CNPJ,
+							        empenho.retornaFavorecido().retornaNome(),
+							        empenho.retornaFavorecido().retornaCargo(),
+									str(idCliente)
+							        ))
+							con.commit()
+
+						#RETORNA O ID DO FAVORECIDO ADICIONADO
+						cursor.execute("SELECT IdFavorecido from Favorecido where Nome = '"+empenho.retornaFavorecido().retornaNome()+"'")
+						IdRetornado = cursor.fetchone()
+						idFavorecido = str(IdRetornado['IdFavorecido'])
+
+						#INSERE EMPENHO
+						sqlquery = "INSERT INTO Empenho(Especie,Orgao,Projeto,Elemento,Licitacao,Processo,DataEmpenho,Valor,NumeroEmpenho,IdFavorecido,Funcao,SubFuncao,Programa,Destinacao,idCliente) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
 
-					data  = datetime.strptime(empenho.dataEmpenho,"%d/%m/%Y").strftime("%Y-%m-%d")
-
-					empenho.valor = reddit['VlEmpenho']
-					empenho.empenho_Numero = reddit['NumEmpenho']
-
-					empenho.funcao = reddit['NumFuncao']+" - "+reddit['DescFuncao']
-					empenho.subFuncao = reddit['NumSubFuncao']+" - "+reddit['DescSubFuncao']
-					empenho.programa = reddit['NumPrograma']+" - "+reddit['DescPrograma']
-					empenho.destinacao = reddit['NumDestinacao']+" - "+reddit['DescDestinacao']
-
-					#INSERINDO FAVORECIDO
-					empenho.insereFavorecido(reddit['DescFornecedor'],reddit['NumCpfCnpjFornecedor'],"=====CARGO TESTE=====")
-
-					cursor = con.cursor()
-					#INSERE NOVO FAVORECIDO CASO NÃO TENHA NO BANCO DE DADOS
-					if(not(cursor.execute("SELECT * from Favorecido where Nome = '" + empenho.retornaFavorecido().retornaNome() + "'"))):
-						sqlquery = "INSERT INTO Favorecido(CPF_CNPJ, Nome, Cargo,idCliente) VALUES (%s, %s, %s, %s);"
 						cursor.execute(sqlquery,(
-						        empenho.retornaFavorecido().CPF_CNPJ,
-						        empenho.retornaFavorecido().retornaNome(),
-						        empenho.retornaFavorecido().retornaCargo(),
-								str(idCliente)
-						        ))
+							empenho.especie,
+							empenho.orgao,
+							empenho.projeto,
+							empenho.elemento,
+							empenho.licitacao,
+							empenho.processo,
+							data,
+							empenho.valor,
+							empenho.numero,
+							idFavorecido,
+							empenho.funcao,
+							empenho.subFuncao,
+							empenho.programa,
+							empenho.destinacao,
+							str(idCliente)
+							))
 						con.commit()
 
-					#RETORNA O ID DO FAVORECIDO ADICIONADO
-					cursor.execute("SELECT IdFavorecido from Favorecido where Nome = '"+empenho.retornaFavorecido().retornaNome()+"'")
-					IdRetornado = cursor.fetchone()
-					idFavorecido = str(IdRetornado['IdFavorecido'])
+						contadorEmpenhos = contadorEmpenhos + 1
+						print("Empenho {}".format (contadorEmpenhos))
+						sys.stdout.write("\033[F")
+						time.sleep(0.1)
 
-					#INSERE EMPENHO
-					sqlquery = "INSERT INTO Empenho(Especie,Orgao,Projeto,Elemento,Licitacao,Processo,DataEmpenho,Valor,NumeroEmpenho,IdFavorecido,Funcao,SubFuncao,Programa,Destinacao,idCliente) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+					except:
+						print("Erro empenho")
+			pagina = pagina +1
+		except:
+			print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+			#return
 
-
-					cursor.execute(sqlquery,(
-						empenho.especie,
-						empenho.orgao,
-						empenho.projeto,
-						empenho.elemento,
-						empenho.licitacao,
-						empenho.processo,
-						data,
-						empenho.valor,
-						empenho.numero,
-						idFavorecido,
-						empenho.funcao,
-						empenho.subFuncao,
-						empenho.programa,
-						empenho.destinacao,
-						str(idCliente)
-						))
-					con.commit()
-
-					contadorEmpenhos = contadorEmpenhos + 1
-					print("Empenho {}".format (contadorEmpenhos))
-					sys.stdout.write("\033[F")
-					time.sleep(0.1)
-
-				except exception:
-
-					print("Erro empenho")
-
-		#return
 
 	con.close()
 
